ab775f9dbcd74034500039564f3f9db708bc8baf
Mark Calder <Mark.Calder@unitestudents.com>
2019-11-19 16:39:17 +0000

    remove todo file

--- todo.js
+++
@@ -1,8 +0,0 @@
-// implement ...
-//  Insert (POST/PUT) endpoint
-//    clean up use of tediousRepository
-//  increase code coverage
-//  README (including software prerequisites)
-// consider
-//  postman test collection
-//  aks and docker manifests
